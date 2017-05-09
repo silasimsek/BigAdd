f139f2fcbf2c2e806d01494ce08d7d5e72490142
silasimsek <gizemsimsek.94@gmail.com>
2017-05-09 19:10:03 +0300

    Update main.c
    - Added output file of lexemes on  text forrm

--- main.c
+++ main.c
@@ -41,7 +41,7 @@ int main() {
     char lexeme[100];
     int i = 0;
     int is_reading_comment = 0, is_reading_string = 0;
-
+    FILE *output_file = fopen("lexemes.txt", "w");
     while(1) {
         char c = (char)fgetc(source_code);
         if (c == EOF){
@@ -49,45 +49,73 @@ int main() {
         }
         if(c == '{' && !is_reading_comment && !is_reading_string){ //don't count parenthesis in strings
             parenthesis_count++;
+            //fputs("{ is a parenthesis\n", output_file);
             is_reading_comment = 1;
             continue;
         } else if (c == '}' && is_reading_comment && !is_reading_string) { //don't count parenthesis in strings
             parenthesis_count++;
+            //fputs("} is a parenthesis\n", output_file);
             is_reading_comment = 0;
             continue;
         }
 
         if(!is_reading_comment){ //we are not reading comment, lets check lexemes and errors
             if(c == '"'){ // WARNING: " “ ” are not same!! print(“str”) is not valid in C!!!
+                if(is_reading_string){
+                    lexeme[i++] = c;
+                    lexeme[i] = '\0';
+                    fprintf(output_file, "%s is a string\n", lexeme);
+                    i = 0;
+                }else{
+                    lexeme[i++] = c;
+                }
                 is_reading_string = !is_reading_string;
                 continue;
             }
+            if(is_reading_string){
+                lexeme[i++] = c;
+            }
+
             if(!is_reading_string) {
                 if((c>=65 && c<=91) || (c>=97 && c<=123) || (c>=48 && c<=57) || (c>=44 && c<=46)
                    || c==34 || c==93 || c==125 || c==32 || c==9 || c==10){
-                    if(c == '[' || c == ']'){
+
+                    if(c == '['){
+                        fputs("[ is a parenthesis\n", output_file);
+                        parenthesis_count++;
+                        continue;
+                    }else if(c == ']'){
+                        fputs("] is a parenthesis\n", output_file);
                         parenthesis_count++;
                         continue;
                     }
+
                     if(c!=' ' && c!='.' && c!='\t' && c!='\n' && c!=','){
                         lexeme[i++] = c;
                     } else {
-                        if (c == '.') {
-                            eol_count++;
-                        }
                         if (i != 0) {
                             lexeme[i] = '\0';
                             for (int j = 0; j < 10; j++) {
                                 if(strcmp(lexeme, keywords[j]) == 0){
                                     keyword_count++;
-                                    printf("%s is a keyword\n", lexeme);
+                                    //printf("%s is a keyword\n", lexeme);
+                                    fprintf(output_file, "%s is a keyword\n", lexeme);
                                     break;
                                 }
-                                if(j == 9 && lexeme != NULL && (lexeme[0]<48 || lexeme[0]>57)){
+                                if(j == 9 &&  (lexeme[0]<48 || lexeme[0]>57)){
                                     identifier_count++;
-                                    printf("%s, is a identifier\n", lexeme );
+                                    fprintf(output_file, "%s is a identifier\n", lexeme);
+                                    //printf("%s is a identifier\n", lexeme );
+                                }else if(j ==9 && lexeme[0]>=48 && lexeme[0]<=57){
+                                    fprintf(output_file, "%s is an integer\n", lexeme);
                                 }
                             }
+                        }
+                        if (c == '.') {
+                            eol_count++;
+                            fputs(". is end of line\n", output_file);
+                        }else if(c == ','){
+                            fputs(", is a comma\n", output_file);
                         }
                         i = 0;
                     }
@@ -103,5 +131,6 @@ int main() {
            eol_count, keyword_count, parenthesis_count, identifier_count);
 
     fclose(source_code);
+    fclose(output_file);
     return 0;
 }
