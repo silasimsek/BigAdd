021b7c399340fa97978769e080bb5f37e2af92a0
Bekir Uzun <bekiruzun8000@gmail.com>
2017-05-10 23:41:40 +0300

    Added more comments

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main() {
     char filename[100];
     filename[0] = '\0';
 
-    while(1) { //until user enters a correct filename
+    while(1) { //loop until user enters a correct filename
         printf("Enter a file name: ");
         int is_filename_acceptable = 0;
         scanf(" %[^\n]s", filename); //to accept spaces in file name too
@@ -20,7 +20,7 @@ int main() {
             } else {
                 printf("Error: Wrong extension!\n");
             }
-        } else {
+        } else { //filename dont have extension, lets add
             int len = (int)strlen(filename);
             filename[len] = '.';
             filename[len + 1] = 'b';
@@ -37,13 +37,13 @@ int main() {
         }
     }
 
-    FILE * source_code = fopen (filename, "r");
-    char lexeme[100];
+    FILE * source_code = fopen (filename, "r"); //lexical analysis output
+    char lexeme[100]; //temporary char array for each lexeme
     int i = 0;
     int is_reading_comment = 0, is_reading_string = 0;
     FILE *output_file = fopen("lexemes.txt", "w");
     while(1) {
-        char c = (char)fgetc(source_code);
+        char c = (char)fgetc(source_code); //read one char from source
         if (c == EOF){
             break;
         }
@@ -52,7 +52,7 @@ int main() {
             //fputs("{ is a parenthesis\n", output_file);
             is_reading_comment = 1;
             continue;
-        } else if (c == '}' && is_reading_comment && !is_reading_string) { //don't count parenthesis in strings
+        } else if (c == '}' && is_reading_comment && !is_reading_string) {
             parenthesis_count++;
             //fputs("} is a parenthesis\n", output_file);
             is_reading_comment = 0;
@@ -61,7 +61,7 @@ int main() {
 
         if(!is_reading_comment){ //we are not reading comment, lets check lexemes and errors
             if(c == '"'){ // WARNING: " “ ” are not same!! print(“str”) is not valid in C!!!
-                if(is_reading_string){
+                if(is_reading_string){ //this means we are end of a string
                     lexeme[i++] = c;
                     lexeme[i] = '\0';
                     fprintf(output_file, "%s is a string\n", lexeme);
@@ -78,7 +78,8 @@ int main() {
 
             if(!is_reading_string) {
                 if((c>=65 && c<=91) || (c>=97 && c<=123) || (c>=48 && c<=57) || (c>=44 && c<=46)
-                   || c==93 || c==125 || c==32 || c==9 || c==10){
+                   || c==93 || c==125 || c==32 || c==9 || c==10){ 
+                   	//accept A-Z a-z 0-9 {} , - . [] {} space \t \n characters in source code
 
                     if(c == '['){
                         fputs("[ is a parenthesis\n", output_file);
@@ -90,23 +91,23 @@ int main() {
                         continue;
                     }
 
-                    if(c!=' ' && c!='.' && c!='\t' && c!='\n' && c!=','){
-                        lexeme[i++] = c;
+                    if(c!=' ' && c!='.' && c!='\t' && c!='\n' && c!=','){ 
+                        lexeme[i++] = c; //still reading a lexeme
                     } else {
-                        if (i != 0) {
+                        if (i != 0) { //check if lexeme is not null/empty
                             lexeme[i] = '\0';
-                            for (int j = 0; j < 10; j++) {
-                                if(strcmp(lexeme, keywords[j]) == 0){
+                            for (int j = 0; j < 10; j++) { //for loop that checks if lexeme is a keyword
+                                if(strcmp(lexeme, keywords[j]) == 0){ //string equality
                                     keyword_count++;
                                     //printf("%s is a keyword\n", lexeme);
                                     fprintf(output_file, "%s is a keyword\n", lexeme);
                                     break;
                                 }
-                                if(j == 9 &&  (lexeme[0]<48 || lexeme[0]>57) && lexeme[0] != '-'){
-                                    identifier_count++;
+                                if(j == 9 &&  (lexeme[0]<48 || lexeme[0]>57) && lexeme[0] != '-'){//end of loop
+                                    identifier_count++;  
                                     fprintf(output_file, "%s is an identifier\n", lexeme);
                                     //printf("%s is a identifier\n", lexeme );
-                                }else if((j ==9 && lexeme[0]>=48 && lexeme[0]<=57) || (j ==9 && lexeme[0]== '-')){
+                                }else if((j == 9 && lexeme[0]>=48 && lexeme[0]<=57) || (j == 9 && lexeme[0]== '-')){
                                     fprintf(output_file, "%s is an integer\n", lexeme);
                                 }
                             }
